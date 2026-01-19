2baede26495509ea00195ff25ba007efeaa33099
krishab555 <kreeshabhandari111@gmail.com>
2026-01-19 15:30:18 +0545

    [200~#Register user using existing email.

---
+++ AutomationExercise/Test1.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import sync_playwright
+import time
+def run():
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=False)
+            page = browser.new_page()
+    
+            page.goto("http://automationexercise.com") #open website
+            time.sleep(2)
+    
+            page.click("text=Signup / Login")   
+            time.sleep(2)
+    
+            name = "Krisha"       #enter name email
+            email = "krisha@123.com"
+    
+            page.fill("input[data-qa='signup-name']", name)
+            page.fill("input[data-qa='signup-email']", email)
+    
+            # Click Signup
+            page.click("button[data-qa='signup-button']")
+            page.wait_for_selector("input[id='password']", timeout=300)
+            print("Signup page loaded")
+            time.sleep(2)
+
+            browser.close()
+
+if __name__ == "__main__":
+    run() 
