31bf6986d6f5ca0ee1be9e7e3c0cf64d2d214b20
krishab555 <kreeshabhandari111@gmail.com>
2026-01-28 11:58:17 +0545

    LoginBeforeCheckout done

--- AutomationExercise/LoginBeforeCheckout.py
+++ AutomationExercise/LoginBeforeCheckout.py
@@ -13,12 +13,46 @@ def run():
         print("Navigated to Signup / Login page")
         time.sleep(2)
 
-        email = "krisha@123.com"
-        password = "Kri123"
-        page.fill("input[data-qa='login-email']", email)
-        page.fill("input[data-qa='login-password']", password)  
+        page.fill("input[data-qa='login-email']", "krisha1769576321@example.com" )
+        page.fill("input[data-qa='login-password']", "Krish@123")
         page.click("button[data-qa='login-button']")
         print("Login successful")
+        time.sleep(2)   
+
+        Add_to_Cart = page.locator("text=Add to cart").first
+        Add_to_Cart.click()
+        print("Add to cart clicked")
+        time.sleep(2)
+        
+        page.click("text= View Cart")
+        print("Navigated to Cart page")
+        time.sleep(2)
+
+        page.click("text= Proceed To Checkout")
+        print("Proceeded to Checkout")
+        time.sleep(2)
+
+        page.click("text=Place Order")
+        print("Order placed successfully")
+        time.sleep(2)   
+
+        page.fill("input[name='name_on_card']", "Krisha Bhandari")
+        page.fill("input[name='card_number']", "4111111111111111")
+        page.fill("input[name='cvc']", "123")       
+        page.fill("input[name='expiry_month']", "12")
+        page.fill("input[name='expiry_year']", "2025")
+        time.sleep(2)
+
+        page.click("button[id='submit']")
+        print("Payment details submitted")
+        time.sleep(3)   
+
+        page.click("text= Delete Account")
+        print("Account deletion Successful ")
+        time.sleep(2)   
+
+        page.click("text=continue")
+        print("Continuing to homepage") 
         time.sleep(2)
 
         browser.close()
